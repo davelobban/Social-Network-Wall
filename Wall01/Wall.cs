6ce902b20494149dbffc7dd8ece44e5618cdbfa8
dave <d2_git@lobban-banff.co.uk>
2019-04-19 20:34:38 +0100

    Refactored out User to assign posts to

--- Wall01/Wall.cs
+++ Wall01/Wall.cs
@@ -1,34 +1,72 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wall01;
 
 namespace Wall01
 {
     public class Wall
     {
 
-        private Post _post;
+        private IList<User> _users;
         private IDateDiff _dateDiff;
 
-        public Wall(IDateDiff dateDiff=null)
+        public Wall(IDateDiff dateDiff = null)
         {
             if (dateDiff == null)
             {
-                dateDiff= new DateDiff();
+                dateDiff = new DateDiff();
             }
 
             _dateDiff = dateDiff;
+
+            _users = new List<User>();
         }
 
-        public void Post(string user, string text)
+        public void Post(string userName, string text)
         {
             var timestamp = DateTime.Now;
-            _post = new Post(user, text, timestamp);
+            var user = GetUser(userName);
+            if (user == null)
+            {
+                user = new User(userName);
+                _users.Add(user);
+            }
+
+            user.AddPost(userName, text, timestamp);
         }
 
-        public Post Read(string alice)
+        private User GetUser(string userName)
         {
-            var post=_post;
+            var user = _users.FirstOrDefault(u => u.Name == userName);
+            return user;
+        }
+
+
+        public Post Read(string userName)
+        {
+            var user = GetUser(userName);
+            var posts = user.Posts;
+            var post = posts.FirstOrDefault();
             post.CalcTimeSince(_dateDiff);
             return post;
         }
     }
+
+    public class User
+    {
+        public string Name { get; }
+        public IList<Post> Posts { get; } = new List<Post>();
+
+        public User(string name)
+        {
+            Name = name;
+        }
+
+        public void AddPost(string userName, string text, DateTime timestamp)
+        {
+            Posts.Add(new Post(Name,text,timestamp));
+        }
+    }
 }
+
--- Wall01Tests/WallTests.cs
+++ Wall01Tests/WallTests.cs
@@ -53,6 +53,45 @@ namespace Tests
 
             Assert.AreEqual(minutesAgo, actual);
         }
+
+
+        [Test]
+        public void Read_1MessagePostedByAliceAnd2ByBob_PostsReturned()
+        {
+            var alicePost1MinutesAgo = "(5 minutes ago)";
+            var alicePost1Text = "I love the weather today";
+            var bobPost1MinutesAgo = "(2 minutes ago)";
+            var bobPost1Text = "Damn! We lost!";
+            var bobPost2MinutesAgo = "(5 minutes ago)";
+            var bobPost2Text = "Good game though.";
+
+            var dateDiffProvider = new Mock<IDateDiff>();
+            dateDiffProvider.Setup(d => d.GetTimeSincePosted(It.Is<Post>(p => p.Text == alicePost1Text))).Returns(alicePost1MinutesAgo);
+            dateDiffProvider.Setup(d => d.GetTimeSincePosted(It.Is<Post>(p => p.Text == bobPost1Text))).Returns(bobPost1MinutesAgo);
+            dateDiffProvider.Setup(d => d.GetTimeSincePosted(It.Is<Post>(p => p.Text == bobPost2Text))).Returns(bobPost2MinutesAgo);
+
+
+            var subject = new Wall(dateDiffProvider.Object);
+
+            
+            subject.Post("Alice", alicePost1Text);
+            subject.Post("Bob", bobPost1Text);
+            subject.Post("Bob", bobPost2Text);
+
+            var minsAgo = alicePost1MinutesAgo;
+            var text = alicePost1Text;
+            var userName = "Alice";
+            AssertPostReturnedByWall(subject, userName, minsAgo, text);
+            AssertPostReturnedByWall(subject, "Bob", bobPost1MinutesAgo, bobPost1Text);
+            //AssertPostReturnedByWall(subject, "Bob", bobPost2MinutesAgo, bobPost2Text);
+        }
+
+        private static void AssertPostReturnedByWall(Wall subject, string userName, string minsAgo, string text)
+        {
+            var actual = subject.Read(userName);
+            Assert.AreEqual(minsAgo, actual.TimeSince);
+            Assert.AreEqual(text, actual.Text);
+        }
     }
 
 
